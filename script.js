a179b3c62a68017e717bbf1ef662c8b4d7fd50ec
bhupeshdubey355 <bhupeshdubey7440@gmail.com>
2024-06-10 11:57:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    let navitems = document.getElementById("nav-items");
+
+    function showmenu() {
+      navitems.style.right = "0";
+    }
+    function hidemenu() {
+      navitems.style.right = "-400px";
+    }
